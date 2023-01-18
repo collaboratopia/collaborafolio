8cc6c105592be06e74cc38424a8c22f3092ebcb7
CJ <83363242+LinuxGamer@users.noreply.github.com>
2023-01-18 20:15:14 +0000

    Update [username].js

--- pages/[username].js
+++ pages/[username].js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Navbar from "../components/Navbar.js";
 
+username={data.username}
+
 export default function Home() {
   return (
     <div className={styles.container}>
