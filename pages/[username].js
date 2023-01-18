b17c923d5496e3538c33a0a395e9f327f2541aab
CJ <83363242+LinuxGamer@users.noreply.github.com>
2023-01-18 20:11:03 +0000

    Update and rename {user}.js to [username].js

--- pages/[username].js
+++ pages/[username].js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>{user} | Collaborafolio</title>
+        <title>user | Collaborafolio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header>
