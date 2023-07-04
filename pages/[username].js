d76d4533abb22dc8c0f196e8faac1a0e90d24160
CJ <83363242+LinuxGamer@users.noreply.github.com>
2023-07-04 19:34:23 +0100

    Update [username].js

--- pages/[username].js
+++ pages/[username].js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Navbar from "../components/Navbar.js";
-//import '../data/[username].json';
+import '../data/{username}.json';
 
 
 export default function Home() {
